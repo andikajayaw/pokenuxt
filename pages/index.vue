c8b4802660510975adceaa252102d90e870fdfbd
LAPTOP-E5999C64\SiNDATA - ANDIKA <andikajayawiguna@gmail.com>
2022-11-30 15:59:57 +0800

    try add Pokemon API

--- pages/index.vue
+++ pages/index.vue
@@ -5,10 +5,10 @@
     >
       <div
         class="rounded border-gray-300 dark:border-gray-700 border-dashed border-2 h-24"
-        v-for="pokemon in resPokemon"
+        v-for="pokemon in data.results"
       >
-        <HelloWorld />
-        <pre>{{ pokemon }}</pre>
+        {{ pokemon.name }}
+        {{ pokemon.url }}
       </div>
     </div>
   </div>
@@ -16,12 +16,7 @@
 
 <script setup>
 import axios from "axios";
-
-const resPokemon = async () => {
-  return await axios
-    .get(`https://pokeapi.co/api/v2/pokemon`)
-    .then((response) => response.results)
-    .catch((error) => alert(error.message));
-};
-console.log(resPokemon());
+const dataPokemon = ref([]);
+const { data } = await useFetch("https://pokeapi.co/api/v2/pokemon");
+console.log(data.results);
 </script>

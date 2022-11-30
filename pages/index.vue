9ebf6d8c41c09c56767287305c2b15a7ab486820
LAPTOP-E5999C64\SiNDATA - ANDIKA <andikajayawiguna@gmail.com>
2022-11-30 16:58:52 +0800

    create card layout and enhance API

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,79 @@
 <template>
   <div>
-    <div
-      class="container mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 pt-6 gap-8"
-    >
-      <div
-        class="rounded border-gray-300 dark:border-gray-700 border-dashed border-2 h-24"
-        v-for="pokemon in data.results"
-      >
-        {{ pokemon.name }}
-        {{ pokemon.url }}
+    <div class="container my-12 mx-auto px-4 md:px-12">
+      <div class="flex flex-wrap -mx-1 lg:-mx-4">
+        <!-- Column -->
+        <div
+          class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3"
+          v-for="pokemon in dataPokemon"
+        >
+          <!-- Article -->
+          <article class="overflow-hidden rounded-lg shadow-lg">
+            <a href="#">
+              <img
+                class="block h-auto"
+                :src="pokemon.sprites.other['official-artwork'].front_default"
+              />
+            </a>
+
+            <header
+              class="flex items-center justify-between leading-tight p-2 md:p-4"
+            >
+              <h1 class="text-lg">
+                <a class="no-underline hover:underline text-black" href="#">
+                  {{ pokemon.name }}
+                </a>
+              </h1>
+              <ul>
+                <li v-for="type in pokemon.types">{{ type.type.name }}</li>
+              </ul>
+            </header>
+          </article>
+          <!-- END Article -->
+        </div>
+        <!-- END Column -->
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
 import axios from "axios";
 const dataPokemon = ref([]);
 const { data } = await useFetch("https://pokeapi.co/api/v2/pokemon");
-console.log(data.results);
+async function getDataPokemon() {
+  await axios
+    .get("https://pokeapi.co/api/v2/pokemon")
+    .then(function (response) {
+      // handle success
+      let res = response.data.results;
+      res.filter(async (x) => {
+        const [resPokemon] = await Promise.all([
+          await axios
+            .get(x.url)
+            .then(function (response) {
+              // handle success
+              console.log(response.data.id);
+              return response.data;
+            })
+            .catch(function (error) {
+              console.log(error);
+            }),
+        ]);
+        console.log(resPokemon);
+        dataPokemon.value.push(resPokemon);
+      });
+      dataPokemon.value.sort((a, b) => {
+        return a.id > b.id ? 1 : -1;
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  //   console.log(dataPokemon.value);
+}
+(async () => {
+  await getDataPokemon();
+})();
 </script>
